13c4295e633e7964c5b759c3605c4911b6a576cc
mel148 <melodygatan@gmail.com>
2023-09-02 00:38:46 -0700

    yippie i finished it!

--- shopping-cart-app/src/context/shop-context.jsx
+++ shopping-cart-app/src/context/shop-context.jsx
@@ -14,6 +14,18 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo.price
+            }
+        }
+
+        return totalAmount;
+    };
+    
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
     };
@@ -22,12 +34,23 @@ export const ShopContextProvider = (props) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}));
     };
 
-    const contextValue = {cartItems, addToCart, removeFromCart};
+    const updateCartItemCount = (newAmount, itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: newAmount}));
+    };
+
+    const contextValue = {
+        cartItems, 
+        addToCart, 
+        removeFromCart,
+        updateCartItemCount,
+        getTotalCartAmount,
+        
+    };
 
     console.log(cartItems)
     return (
         <ShopContext.Provider value={contextValue}>
-        {props.children}
+            {props.children}
         </ShopContext.Provider>
     );
 };
